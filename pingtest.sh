dc383e3e6a15f47d5f41daebeb462e99dedc0067
pmnikodem <pmnikodem@gmail.com>
2018-12-14 11:22:23 -0600

    initial push

---
+++ pingtest.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+set -e
+TARGETFILE=targets
+FPING=`which fping` &&
+TRACEROUTE=`which traceroute` &&
+PRINTF=`which printf`
+
+function pingtest()
+{
+	TARGET=$1
+	LOGFILE=logs/$TARGET.log
+	RESFILE=logs/$TARGET-result.txt
+	touch $RESFILE
+	#export TZ='America/Chicago'
+	export TIMESTAMP=`date +%s`
+	LASTCHANGE=`tail -n 1 $RESFILE`
+
+	grep -q -i failed $RESFILE
+
+	if [ $? = 0 ]; then
+		# if last result was failed, attempt to reconnect:
+		$FPING -c3 $TARGET &>/dev/null
+		# if reconnect succeeded, log to file and record length of outage:
+		if [ $? = 0 ]; then
+			echo 'succeeded' > $RESFILE
+			echo $TIMESTAMP >> $RESFILE
+			$PRINTF "`date -d @$TIMESTAMP`" >> $LOGFILE
+			let "TIMEDIFF = $TIMESTAMP - $LASTCHANGE"
+			$PRINTF ' - reconnected after %s seconds\n' $TIMEDIFF >> $LOGFILE
+		fi
+	elif [ $? != 0 ]; then
+		# if last result was succeeded, test whether we are still OK:
+		$FPING -c3 $TARGET &>/dev/null
+		# if current test failed, log to file and record timestamp of initial failure:
+		if [ $? != 0 ]; then
+			echo 'failed' > $RESFILE
+			echo $TIMESTAMP >> $RESFILE
+			echo '' >> $LOGFILE
+			$PRINTF "`date -d @$TIMESTAMP`" >> $LOGFILE
+			$PRINTF ' - lost connection to %s\n' $TARGET >> $LOGFILE
+			$PRINTF '=%.0s' {1..70} >> $LOGFILE
+			$PRINTF '\n' >> $LOGFILE
+			$TRACEROUTE -I $TARGET -m12 -I >> $LOGFILE
+			$PRINTF '=%.0s' {1..70} >> $LOGFILE
+			$PRINTF '\n' >> $LOGFILE
+		fi
+	fi
+}
+
+while true
+do
+	while read TARGET
+	do
+		pingtest $TARGET &
+	done < $TARGETFILE
+	sleep 5
+done
